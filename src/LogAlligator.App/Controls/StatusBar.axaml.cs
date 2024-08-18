d2024230d37c29a877fe7a9bc564a4547f0d67a8
Kyriet <kyriet.sebastian@gmail.com>
2024-08-19 01:21:05 +0200

    Add StatusBar

---
+++ src/LogAlligator.App/Controls/StatusBar.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LogAlligator.App.Controls;
+
+public partial class StatusBar : UserControl
+{
+    public StatusBar()
+    {
+        InitializeComponent();
+    }
+}
+
