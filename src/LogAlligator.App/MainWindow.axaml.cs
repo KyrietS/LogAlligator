f706b4c322eb51e4a987954f37aad8d7e22d529c
Kyriet <kyriet.sebastian@gmail.com>
2024-08-20 01:10:21 +0200

    Added open file dialog

--- src/LogAlligator.App/MainWindow.axaml.cs
+++ src/LogAlligator.App/MainWindow.axaml.cs
@@ -1,11 +1,37 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Platform.Storage;
 using Avalonia.Styling;
+using LogAlligator.App.Controls;
 
 namespace LogAlligator.App;
 
 public partial class MainWindow : Window
 {
+    private async void MenuBar_Open_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var files = await this.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Open log file", AllowMultiple = false, FileTypeFilter = null
+        });
+
+        if (files.Count < 1)
+        {
+            return;
+        }
+
+        var file = files[0];
+        var fileView = new FileView();
+        var fileTab = new TabItem
+        {
+            Header = file.Name,
+            Content = fileView
+        };
+        FileTabs.SelectedIndex = FileTabs.Items.Add(fileTab);
+        fileView.LoadFile(file.Path.AbsolutePath);
+    }
+
     public MainWindow()
     {
         InitializeComponent();
