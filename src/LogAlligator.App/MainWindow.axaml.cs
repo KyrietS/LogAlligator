69e5f973ade86d816554c62c4046ff9e0eebe369
Kyriet <kyriet.sebastian@gmail.com>
2024-08-21 00:55:04 +0200

    Fix ToolTip theme not being updated

--- src/LogAlligator.App/MainWindow.axaml.cs
+++ src/LogAlligator.App/MainWindow.axaml.cs
@@ -5,6 +5,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
 using Avalonia.Media;
 using Avalonia.Platform.Storage;
 using Avalonia.Styling;
@@ -18,7 +19,8 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
-
+        
+        Application.Current!.ActualThemeVariantChanged += (_, _) => RefreshAllToolTips();
         FileTabs.Background = new SolidColorBrush(Colors.Transparent);
         FileTabs.AddHandler(DragDrop.DragOverEvent, OnDragOver);
         FileTabs.AddHandler(DragDrop.DropEvent, OnDrop);
@@ -103,5 +105,28 @@ public partial class MainWindow : Window
         var fileTab = new TabItem { Header = file.Name, Content = new FileView{FilePath = file.Path} };
         ToolTip.SetTip(fileTab, file.Path.AbsolutePath);
         FileTabs.SelectedIndex = FileTabs.Items.Add(fileTab);
+    }
+
+    /// <summary>
+    /// When tooltip is displayed and after that the theme is changes, the theme of the tooltip is not updated.
+    /// To fix this I set all tooltips again to force the update.
+    /// </summary>
+    private void RefreshAllToolTips()
+    {
+        // Iterate over all logical descendants of the window
+        foreach (var child in this.GetLogicalDescendants())
+        {
+            // Only a Control can have a ToolTip
+            if (child is Control control)
+            {
+                var toolTip = ToolTip.GetTip(control);
+                if (toolTip is string toolTipString)
+                {
+                    // Set a new Tip with the same value to force the ToolTip to update
+                    // Note: you cannot use ToolTip.SetTip(control, toolTipString) because it will not work
+                    ToolTip.SetTip(control, new String(toolTipString));
+                }
+            }
+        }
     }
 }
